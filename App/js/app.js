0393d9606e2c40051775d594e3167a49fb107ac4
wandzi <wandzel.mateusz@yahoo.com>
2019-11-18 21:15:48 +0100

    feat(PrinterClass): Done writing Printer Class Constructor. Started working on change background color method that depends on the module that printer has.

--- App/js/app.js
+++ App/js/app.js
@@ -1,24 +1,63 @@
 const printersList = document.querySelector('#printers-status-list');
 
+
+class Printer {
+    constructor(printer_model, printer_number, printing_file_name, printing_hours){
+        this.printerModel = printer_model;
+        this.printerNumber = printer_number;
+        this.printingFileName = printing_file_name;
+        this.printingHours = printing_hours;
+    }
+    containerColor(){
+        let modulColor;
+
+        switch (this.printer_model) {
+            case ("One"):
+                modulColor = 'one-printer';
+                break;
+            case ("Double P255"):
+                modulColor = 'double-printer';
+                break;
+            case ("F340 - Pro"):
+                modulColor = 'f340-pro-printer';    
+                break;
+            case ("F340 - HT"):
+                modulColor = 'f340-ht-printer';
+                break;
+            case ("F340 - HT Max"):
+                modulColor = 'f340-htmax-printer';
+                break;
+            case ("FormLab Form2"):
+                modulColor = 'formlab-printer';
+                break;
+        }
+        return modulColor;
+    }
+}
+
 //Generating UI printers list from DataBase
 const SetupPrintersList = (data) => {
+
     let list = '';
     data.forEach(doc => {
-        const printer = doc.data();
+        const dataBase = doc.data();
+        const printer = new Printer(dataBase.printer_model, dataBase.printer_number, dataBase.printing_file_name, dataBase.printing_hours);
+
         const printerContainer = `
-        <div class="printer-container">
-            <p class="bigger-font">${printer.printer_model} - numer: ${printer.printer_number}</p>
-            <p class="bigger-font">${printer.printing_file_name}</p>
+        <div class="printer-container ${printer.containerColor}">
+            <p class="bigger-font ">${printer.printerModel} - numer: ${printer.printerNumber}</p>
+            <p class="bigger-font">${printer.printingFileName}</p>
             <div class="w3-light-grey w3-round-xlarge">
-                <div class="w3-container w3-blue w3-round-xlarge">
+                <div class="w3-container w3-blue w3-round-xlarge progressBar">
                 </div>
             </div> 
-            <p class="smaller-font" id="">${printer.printing_hours}h</p>
+            <p class="smaller-font" id="">${printer.printingHours}h</p>
         </div>
         `;      
+
         list+= printerContainer;
     });
-
+    
     printersList.innerHTML = list;
 }
 
