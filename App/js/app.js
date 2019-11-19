77368e013292f5f2d587bb992b1757e99831d86c
wandzi <wandzel.mateusz@yahoo.com>
2019-11-19 21:25:51 +0100

    fix(Modal): It was possible to drag the modal to the left.

--- App/js/app.js
+++ App/js/app.js
@@ -1,6 +1,5 @@
 const printersList = document.querySelector('#printers-status-list');
 
-
 class Printer {
     constructor(printer_model, printer_number, printing_file_name, printing_hours){
         this.printerModel = printer_model;
@@ -8,30 +7,31 @@ class Printer {
         this.printingFileName = printing_file_name;
         this.printingHours = printing_hours;
     }
+    
     containerColor(){
-        let modulColor;
-
-        switch (this.printer_model) {
+        let moduleColor;
+        
+        switch (this.printerModel) {
             case ("One"):
-                modulColor = 'one-printer';
+                moduleColor = 'one-printer';
                 break;
             case ("Double P255"):
-                modulColor = 'double-printer';
+                moduleColor = 'double-printer';
                 break;
             case ("F340 - Pro"):
-                modulColor = 'f340-pro-printer';    
+                moduleColor = 'f340-pro-printer';    
                 break;
             case ("F340 - HT"):
-                modulColor = 'f340-ht-printer';
+                moduleColor = 'f340-ht-printer';
                 break;
             case ("F340 - HT Max"):
-                modulColor = 'f340-htmax-printer';
+                moduleColor = 'f340-htmax-printer';
                 break;
             case ("FormLab Form2"):
-                modulColor = 'formlab-printer';
+                moduleColor = 'formlab-printer';
                 break;
         }
-        return modulColor;
+        return moduleColor;
     }
 }
 
@@ -42,9 +42,10 @@ const SetupPrintersList = (data) => {
     data.forEach(doc => {
         const dataBase = doc.data();
         const printer = new Printer(dataBase.printer_model, dataBase.printer_number, dataBase.printing_file_name, dataBase.printing_hours);
+        const moduleColor = printer.containerColor();
 
         const printerContainer = `
-        <div class="printer-container ${printer.containerColor}">
+        <div class="printer-container ${moduleColor}">
             <p class="bigger-font ">${printer.printerModel} - numer: ${printer.printerNumber}</p>
             <p class="bigger-font">${printer.printingFileName}</p>
             <div class="w3-light-grey w3-round-xlarge">
